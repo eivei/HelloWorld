525e43fecc3dfd94261c53d88d56f70305f4ac12
eivei <43842071+eivei@users.noreply.github.com>
2018-10-04 09:39:20 +0200

    Update HelloWorldServiceProvider.php

--- src/Providers/HelloWorldServiceProvider.php
+++ src/Providers/HelloWorldServiceProvider.php
@@ -8,9 +8,10 @@
     class HelloWorldServiceProvider extends ServiceProvider
     {
      
-        /**
-         * Register the service provider.
-         */
+        public function register()
+        {
+            $this->getApplication()->register(HelloWorldRouteServiceProvider::class);
+        }
      
         public function register()
         {
